980608d89a5d5f852bf766ffc8db0b0e96c9d312
Enno Pettinga <dedicationists@gmail.com>
2017-09-11 13:42:09 +0200

    Extra Exercises

---
+++ Extra Exercises/Extra Exercises/Extra Exercise 1_7.py
@@ -0,0 +1,11 @@
+maanden = ['Jan', 'Feb', 'Mrt', 'Mei']
+maanden.insert (3,'Apr')
+maanden.append ('Jun')
+maanden.remove ('Jun')
+maanden.remove ('Feb')
+print (maanden)
+maanden.reverse ()
+print (maanden)
+maanden.sort ()
+print (maanden)
+
