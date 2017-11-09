6f8c12c62d477a4fd80060b1eef3f792a2546126
Enno Pettinga <dedicationists@gmail.com>
2017-11-09 12:14:47 +0100

    Finale versie, alle PE's gecheck op werking

--- Practice Exercises/Practice Exercises/Practice Exercise 6_4.py
+++ Practice Exercises/Practice Exercises/Practice Exercise 6_4.py
@@ -2,8 +2,9 @@ studentencijfers = [[95, 92, 86], [66, 75, 54], [89, 72, 100], [34, 0, 0]]
 def StudentGemiddelde(studentencijfers):
     GemiddeldeLijst = []
     for studenten in studentencijfers:
-        for cijfers in studenten:
-            gemiddelde = sum(cijfers) / len(cijfers)
-            GemiddeldeLijst.append(gemiddelde)
-print(StudentGemiddelde(studentencijfers))
+        gemiddelde = sum(studenten) / len(studenten)
+        GemiddeldeLijst.append(gemiddelde)
+    print(GemiddeldeLijst)
+
+StudentGemiddelde(studentencijfers)
 
