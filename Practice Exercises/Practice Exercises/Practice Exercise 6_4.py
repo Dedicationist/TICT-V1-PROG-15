a6af86864abb4032b37b4fa37aded44c42f051d6
Enno Pettinga <dedicationists@gmail.com>
2017-11-09 12:44:04 +0100

    PE 6_4 fix

--- Practice Exercises/Practice Exercises/Practice Exercise 6_4.py
+++ Practice Exercises/Practice Exercises/Practice Exercise 6_4.py
@@ -1,10 +1,20 @@
 studentencijfers = [[95, 92, 86], [66, 75, 54], [89, 72, 100], [34, 0, 0]]
+
 def StudentGemiddelde(studentencijfers):
+    global GemiddeldeLijst
     GemiddeldeLijst = []
     for studenten in studentencijfers:
         gemiddelde = sum(studenten) / len(studenten)
         GemiddeldeLijst.append(gemiddelde)
-    print(GemiddeldeLijst)
+    return GemiddeldeLijst
 
-StudentGemiddelde(studentencijfers)
+def AlgemeenGemiddelde(GemiddeldeLijst):
+    gemiddelde = sum(GemiddeldeLijst) / len(GemiddeldeLijst)
+    return gemiddelde
+
+
+
+
+print(StudentGemiddelde(studentencijfers))
+print(AlgemeenGemiddelde(GemiddeldeLijst))
 
